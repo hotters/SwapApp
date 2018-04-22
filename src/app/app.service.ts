fd61cececc04310f70844bc643522add62bf9cbf
Mint <mint.devstudio@gmail.com>
2018-04-22 14:58:44 +1000

    styles

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,24 +3,64 @@ import { HttpClient } from '@angular/common/http';
 import { Name } from './models/names.model';
 
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
 export class AppService {
+
+	data = [
+		{
+			'id': 1,
+			'name': 'John'
+		},
+		{
+			'id': 2,
+			'name': 'William'
+		},
+		{
+			'id': 3,
+			'name': 'Olivia'
+		},
+		{
+			'id': 4,
+			'name': 'Mia'
+		},
+		{
+			'id': 5,
+			'name': 'Benjamin'
+		},
+		{
+			'id': 6,
+			'name': 'Michael'
+		},
+		{
+			'id': 7,
+			'name': 'Emily'
+		},
+		{
+			'id': 8,
+			'name': 'Liam'
+		},
+		{
+			'id': 9,
+			'name': 'Isabella'
+		},
+		{
+			'id': 10,
+			'name': 'John'
+		}
+	];
+
 
 	constructor(
 		private http: HttpClient,
 	) { }
 
 	getNames() {
-		return this.http.get<Array<Name>>('assets/names.json').pipe(
-			// map(names => {
-			// 	for (let i = 11; i < 10000; i++) {
-			// 		names.push({id: '' + i, name: 'a' + i});
-
-			// 	}
-			// 	return names;
-			// }),
+		// return this.http.get<Array<Name>>('assets/names.json').pipe(
+		// for stackblitz.com
+		return new Observable<Array<Name>>(obs => obs.next(this.data)).pipe(
 			map(names => {
 				const result = {};
 				names.forEach(item => result[item.id] = {name: item.name});
