54fb697ebb5381f8ded333ba04b049998d216478
Mint <mint.devstudio@gmail.com>
2018-04-22 15:11:33 +1000

    another styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.sass'],
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 
